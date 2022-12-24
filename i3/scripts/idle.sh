5a75736e6259295bcea8307db1f3f01c45ff37fb
wolandark <107309764+wolandark@users.noreply.github.com>
2022-12-24 15:15:47 +0330

    Add files via upload

---
+++ i3/scripts/idle.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+xidlehook --not-when-fullscreen --not-when-audio  --timer 900 "betterlockscreen -l" ''
+
+
+# xidlehook \
+#     --not-when-fullscreen \
+#     --not-when-audio \
+#     --timer 900 "betterlockscreen -l" '' \
+
